bc208d997210ac431573ca7464aeb723b892612d
akashkanade9182 <akashkanade235@gmail.com>
2023-04-05 15:41:46 +0530

    file change

--- frontend/src/pages/TransactionsApp.jsx
+++ frontend/src/pages/TransactionsApp.jsx
@@ -8,8 +8,8 @@ const TransactionsApp = () => {
         <h1>Last Trasections</h1>
         <h1>Sell All</h1>
       </div>
-      <div className='card'>
-          {/* <div>
+      <div className='cards'>
+          <div>
             <img className="avatar" src="https://bit.ly/sage-adebayo" alt="" />
 
           </div>
@@ -29,7 +29,53 @@ const TransactionsApp = () => {
               <div>
                 Received
               </div>
-        </div> */}
+        </div>
+      </div>
+      <div className='cards'>
+          <div>
+            <img className="avatar" src="https://bit.ly/code-beast" alt="" />
+
+          </div>
+          <div className='firstbox'>
+              <div>
+                28 Feb 2023
+              </div>
+              <div>
+                06.23PM
+              </div>
+
+          </div>
+        <div className="secondbox">
+              <div style={{color:"red"}}>
+                -$9.00
+              </div>
+              <div>
+                Outgoing
+              </div>
+        </div>
+      </div>
+      <div className='cards'>
+          <div>
+            <img className="avatar" src="https://bit.ly/prosper-baba" alt="" />
+
+          </div>
+          <div className='firstbox'>
+              <div>
+                28 Feb 2023
+              </div>
+              <div>
+                06.23PM
+              </div>
+
+          </div>
+        <div className="secondbox">
+              <div >
+                +$11.00
+              </div>
+              <div>
+                Received
+              </div>
+        </div>
       </div>
     </div>
   )
